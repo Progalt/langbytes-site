614f31af608d1b526762e17d0acad189bde28bc5
Progalt <henryrgronow@gmail.com>
2024-03-13 16:46:10 +0000

    Working on home page

--- src/app/Components/Tooltip.js
+++ src/app/Components/Tooltip.js
@@ -15,7 +15,7 @@ export function Tooltip({ text, children  }) {
                 {children}
             </div>
             {showTooltip && (
-                <div className="absolute x-20 bg-black text-white text-sm py-1 px-2 rounded whitespace-nowrap top-0 left-1/2 transform -translate-x-1/2 translate-y-full">
+                <div className="absolute x-20 bg-black/50 text-white text-sm py-1 px-2 rounded whitespace-nowrap top-0 left-1/2 transform -translate-x-1/2 translate-y-full">
                 {text}
                 </div>
             )}
--- src/app/page.js
+++ src/app/page.js
@@ -4,10 +4,15 @@ import { TaskSnippet, supabase } from "./Components/TaskSnippet";
 import { IoPerson } from "react-icons/io5";
 import { DifficultyButton } from "./Components/DifficultyButton";
 import { Modal } from "./Components/Modal";
+import { TiArrowBackOutline } from "react-icons/ti";
+import { MdBackHand } from "react-icons/md";
+import { LanguageButton } from "./Components/LanguageButton";
+import { Inter } from "next/font/google";
 
 function getRandomInteger(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
 
 export default function Home() {
 
@@ -15,6 +20,7 @@ export default function Home() {
   const [ questionId, setQuestionID ] = useState(0);
   const [ difficulty, setDifficulty ] = useState("Medium");
   const [ questionCount, setQuestionCount] = useState(null);
+  const [ selectedLanguages, setSelectedLanguages ] = useState([ "Python", "JavaScript" ]);
 
   useEffect( () => {
     const urlParams = new URLSearchParams(window.location.search);
@@ -91,62 +97,99 @@ export default function Home() {
     setShowQuestion(true);
   }
 
+  const possibleLanguages = [ "Python", "JavaScript", "Haskell", "C++", "C#", "Assembly", "Java" ];
+  const numColumns = 4; 
+  const numRows = Math.ceil(possibleLanguages.length / numColumns);
+
   return (
       <main className="p-5 w-full h-screen flex flex-col justify-center items-center">
         <div className="w-[60%]">
           { showQuestion && 
             <div className="flex flex-col justify-center items-center">
               <TaskSnippet id={questionId} difficulty={difficulty} />
-              <button
-                onClick={() => {
-                  getNewID();
-                }}
-                className="mt-4 shadow-[0_0px_30px_0] shadow-indigo-500/50 hover:shadow-red-500/50 bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-semibold rounded-full p-[2px] transform transition-all duration-300 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-500 hover:via-red-500 hover:to-indigo-500">
-                  <span className="flex w-full bg-gray-900 text-white rounded-full p-2 px-4">
-                    Give me a question
-                  </span>
-                </button>
+              <div className="flex items-center justify-center">
+                <button
+                  onClick={() => {
+                    getNewID();
+                  }}
+                  className="mt-4 shadow-[0_0px_30px_0] shadow-indigo-500/50 hover:shadow-red-500/50 bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-semibold rounded-full p-[2px] transform transition-all duration-300 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-500 hover:via-red-500 hover:to-indigo-500">
+                    <span className="flex w-full bg-gray-900 text-white rounded-full p-2 px-4">
+                      Give me a question
+                    </span>
+                  </button>
+                </div>
             </div>
           } 
           {
             !showQuestion && 
             <div className="flex flex-col justify-center items-center">
-              <h1 className="text-3xl mb-24">wadasdd</h1>
-              <div className="flex flex-row justify-center items-center mb-4">
-                <DifficultyButton difficulty={"Easy"} selected={difficulty} setDifficulty={updateDifficulty}/>
-                <DifficultyButton difficulty={"Medium"} selected={difficulty}  setDifficulty={updateDifficulty}/>
-                <DifficultyButton difficulty={"Hard"} selected={difficulty}  setDifficulty={updateDifficulty}/>
-              </div>
-              
-              {questionCount !== null && 
-                <div className="flex flex-row justify-center items-center">
-                <button 
-                onClick={() => {
-                  getNewID();
-                }}
-                className="shadow-[0_0px_30px_0] shadow-indigo-500/50 hover:shadow-red-500/50 bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-semibold rounded-full p-[2px] transform transition-all duration-300 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-500 hover:via-red-500 hover:to-indigo-500">
-                  <span className="flex w-full bg-gray-900 text-white rounded-full p-2 px-4">
-                    Give me a question
+              <section className="mb-14">
+                <h1 className="text-6xl">
+                  Quickly get questions to test your 
+                  <span className="text-glow text-indigo-100"> coding skills.
                   </span>
-                </button>
-
+                </h1>
+               
+                <h2 className="text-xl mt-6">Select a difficulty or enter a playlist and start coding!</h2>
+              </section>
+              <div className="w-full border-2 border-slate-800 mb-6 rounded-xl">
+                <div className="relative flex flex-row justify-between items-center h-full mb-8">
+                  <button className="w-full bg-slate-800 m-1 rounded-lg h-8">Difficulty</button>
+                  <button className="w-full bg-slate-800 m-1 rounded-lg h-8">Playlist</button>
                 </div>
-              }
+                <div className="flex flex-row justify-center items-center">
+                  <DifficultyButton difficulty={"Easy"} selected={difficulty} setDifficulty={updateDifficulty}/>
+                  <DifficultyButton difficulty={"Medium"} selected={difficulty}  setDifficulty={updateDifficulty}/>
+                  <DifficultyButton difficulty={"Hard"} selected={difficulty}  setDifficulty={updateDifficulty}/>
+                </div>
+                  <div className="mt-4 mb-8">
+                       {
+                        Array.from({ length: numRows }).map((_, rowIndex) => (
+                        <div key={rowIndex} className="mt-2 flex justify-center items-center w-full gap-4">
+                        {
+                          possibleLanguages.slice(rowIndex * numColumns, (rowIndex + 1) * numColumns).map((lang, index) => {
+                              return <LanguageButton key={lang} language={lang} selectedLanguages={selectedLanguages} setSelectedLanguages={setSelectedLanguages}></LanguageButton>
+                            })
+                        }
+                        </div>
+                        ))
+                    }
+                  </div>
+                  {questionCount !== null && 
+                    <div className="flex flex-row justify-center items-center mb-6">
+                  
+                    <button 
+                    onClick={() => {
+                      getNewID();
+                    }}
+                    className="shadow-[0_0px_30px_0] shadow-indigo-500/50 hover:shadow-red-500/50 bg-gradient-to-r from-purple-500 to-indigo-500 text-white font-semibold rounded-full p-[2px] transform transition-all duration-300 hover:scale-110 hover:bg-gradient-to-r hover:from-purple-500 hover:via-red-500 hover:to-indigo-500">
+                      <span className="flex w-full bg-gray-900 text-white rounded-full p-2 px-4">
+                        Give me a question
+                      </span>
+                    </button>
+
+                    </div>
+                  }
+              </div>
+
+             
+              {/* <hr className="w-[80%] border border-slate-800 mb-2"></hr> */}
+              {/* <div className=" mb-14 w-[60%]">
+                <input id="playlist" type="text" name="playlist" placeholder="playlist ID" required
+                  className="mt-4 w-full rounded-xl h-10 outline-2 bg-[#212536] placeholder-slate-200 text-white p-2 border-transparent focus:ring-transparent focus:border-transparent focus:outline focus:outline-indigo-500 focus:outline-offset-2 focus:shadow-[0_0px_30px_0] focus:shadow-indigo-500/50 transition-shadow duration-200" />
+              </div> */}
+              
+              
 
             </div>
             
           }
         </div>
-        {/* <div className="absolute top-0 right-0 mt-4 mr-10">
-          <div className="w-12 h-12 bg-indigo-500 rounded-full flex justify-center items-center">
-            <IoPerson className="text-2xl"/>
-          </div>
-        </div> */}
 
         <footer className="absolute bottom-5">
           <div className="flex flex-row justify-center items-center">
             <p className="text-slate-500">You can view the source</p>
-            <a href="https://github.com/Progalt/portfolio-react" className="ml-1 text-indigo-500 text-glow hover:text-white transition-all duration-150">here</a>
+            <a href="https://github.com/Progalt/portfolio-react" className="ml-1 text-indigo-100 text-glow hover:text-white transition-all duration-150">here</a>
           </div>
         </footer>
       </main>
